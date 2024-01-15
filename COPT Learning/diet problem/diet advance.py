913ea24dfa3d158668acf606dfb09dc0c01a7ecb
chiangwyz <1119027571@qq.com>
2024-01-15 18:21:11 +0800

    bugfix: fix addconstrs

--- COPT Learning/diet problem/diet advance.py
+++ COPT Learning/diet problem/diet advance.py
@@ -13,86 +13,92 @@ model = env.createModel(name="diet_advanced")
 
 # Constraints info. of nutrition
 nutrition, minNutrition, maxNutrition = multidict({
-    'Vitamin A':  [700, 10000],
-    'Vitamin C':  [700, 10000],
+    'Vitamin A': [700, 10000],
+    'Vitamin C': [700, 10000],
     'Vitamin B1': [700, 10000],
     'Vitamin B2': [700, 10000]})
 
 # Cost info. of foods (obj coef)
 foods, cost = multidict({
     'BEEF': 3.19,
-    'CHK':  2.59,
+    'CHK': 2.59,
     'FISH': 2.29,
-    'HAM':  2.89,
-    'MCH':  1.89,
-    'MTL':  1.99,
-    'SPG':  1.99,
-    'TUR':  2.49})
+    'HAM': 2.89,
+    'MCH': 1.89,
+    'MTL': 1.99,
+    'SPG': 1.99,
+    'TUR': 2.49})
 
 # Nutrition values for the foods
 nutritionValues = {
-    ('BEEF', 'Vitamin A'):  60,
-    ('BEEF', 'Vitamin C'):  20,
+    ('BEEF', 'Vitamin A'): 60,
+    ('BEEF', 'Vitamin C'): 20,
     ('BEEF', 'Vitamin B1'): 10,
     ('BEEF', 'Vitamin B2'): 15,
-    ('CHK', 'Vitamin A'):   8,
-    ('CHK', 'Vitamin C'):   0,
-    ('CHK', 'Vitamin B1'):  20,
-    ('CHK', 'Vitamin B2'):  20,
-    ('FISH', 'Vitamin A'):  8,
-    ('FISH', 'Vitamin C'):  10,
+    ('CHK', 'Vitamin A'): 8,
+    ('CHK', 'Vitamin C'): 0,
+    ('CHK', 'Vitamin B1'): 20,
+    ('CHK', 'Vitamin B2'): 20,
+    ('FISH', 'Vitamin A'): 8,
+    ('FISH', 'Vitamin C'): 10,
     ('FISH', 'Vitamin B1'): 15,
     ('FISH', 'Vitamin B2'): 10,
-    ('HAM', 'Vitamin A'):   40,
-    ('HAM', 'Vitamin C'):   40,
-    ('HAM', 'Vitamin B1'):  35,
-    ('HAM', 'Vitamin B2'):  10,
-    ('MCH', 'Vitamin A'):   15,
-    ('MCH', 'Vitamin C'):   35,
-    ('MCH', 'Vitamin B1'):  15,
-    ('MCH', 'Vitamin B2'):  15,
-    ('MTL', 'Vitamin A'):   70,
-    ('MTL', 'Vitamin C'):   30,
-    ('MTL', 'Vitamin B1'):  15,
-    ('MTL', 'Vitamin B2'):  15,
-    ('SPG', 'Vitamin A'):   25,
-    ('SPG', 'Vitamin C'):   50,
-    ('SPG', 'Vitamin B1'):  25,
-    ('SPG', 'Vitamin B2'):  15,
-    ('TUR', 'Vitamin A'):   60,
-    ('TUR', 'Vitamin C'):   20,
-    ('TUR', 'Vitamin B1'):  15,
-    ('TUR', 'Vitamin B2'):  10}
+    ('HAM', 'Vitamin A'): 40,
+    ('HAM', 'Vitamin C'): 40,
+    ('HAM', 'Vitamin B1'): 35,
+    ('HAM', 'Vitamin B2'): 10,
+    ('MCH', 'Vitamin A'): 15,
+    ('MCH', 'Vitamin C'): 35,
+    ('MCH', 'Vitamin B1'): 15,
+    ('MCH', 'Vitamin B2'): 15,
+    ('MTL', 'Vitamin A'): 70,
+    ('MTL', 'Vitamin C'): 30,
+    ('MTL', 'Vitamin B1'): 15,
+    ('MTL', 'Vitamin B2'): 15,
+    ('SPG', 'Vitamin A'): 25,
+    ('SPG', 'Vitamin C'): 50,
+    ('SPG', 'Vitamin B1'): 25,
+    ('SPG', 'Vitamin B2'): 15,
+    ('TUR', 'Vitamin A'): 60,
+    ('TUR', 'Vitamin C'): 20,
+    ('TUR', 'Vitamin B1'): 15,
+    ('TUR', 'Vitamin B2'): 10}
 
-#Add variables
+# Add variables
 buy = model.addVars(foods)
 
 var_show = buy['BEEF']
-print("决策变量BEEF的名称：",var_show.name)
-print("决策变量BEEF的变量类型：",var_show.getType())
-print("模型中决策变量的数量：",model.getAttr(attrname="Cols"))
+print("决策变量BEEF的名称：", var_show.name)
+print("决策变量BEEF的变量类型：", var_show.getType())
+print("模型中决策变量的数量：", model.getAttr(attrname="Cols"))
 
-#Add constraints
-#Add Lower Bound Constraint
-model.addConstrs(quicksum(nutritionValues[(f,n)] * buy[f] for f in foods)
-                 >= minNutrition[n] for n in nutrition)
-#Add Upper Bound Constraint
-model.addConstrs(quicksum(nutritionValues[(f,n)] * buy[f] for f in foods)
-                 <= maxNutrition[n] for n in nutrition)
+# Add constraints
+# Add Lower Bound Constraint
+# model.addConstrs(quicksum(nutritionValues[(f, n)] * buy[f] for f in foods)
+#                  >= minNutrition[n] for n in nutrition)
+# # Add Upper Bound Constraint
+# model.addConstrs(quicksum(nutritionValues[(f, n)] * buy[f] for f in foods)
+#                  <= maxNutrition[n] for n in nutrition)
+
+for n in nutrition:
+    model.addConstr(quicksum(nutritionValues[(f, n)] * buy[f] for f in foods), COPT.GREATER_EQUAL, minNutrition[n],
+                    name=n+"_LB")
+    model.addConstr(quicksum(nutritionValues[(f, n)] * buy[f] for f in foods), COPT.LESS_EQUAL, maxNutrition[n],
+                    name=n+"_UB")
 print("约束数量：", model.getAttr(attrname="rows"))
 
-#Set Objective--quicksum()
-model.setObjective(quicksum(cost[f]*buy[f] for f in foods), sense=COPT.MINIMIZE)
+# Set Objective--quicksum()
+model.setObjective(quicksum(cost[f] * buy[f] for f in foods), sense=COPT.MINIMIZE)
 
-#solve the problem
+# solve the problem
 model.solve()
 
-#Analyze solution
+# Analyze solution
 if model.status == COPT.OPTIMAL:
     # Optimal objective value
     print("最小食谱构建费用：{:.4f}".format(model.objval))
     allvars = model.getVars()
-    #Variable value
+    # Variable value
     print("\nValue of each variable:\n")
     for var in allvars:
         print("决策变量{0},最优值为 {1:.4f}，基状态为：{2}".format(var.name, var.x, var.basis))
@@ -104,7 +110,7 @@ for var in allvars:
 
 # constraints slack and dual
 constr_list = model.getConstrs()
-allslacks =model.getSlacks()
+allslacks = model.getSlacks()
 allduals = model.getDuals()
 for (constr, dual, slack) in zip(constr_list, allduals, allslacks):
     print("约束{0}，对偶变量的值{1:.4f}，松弛变量的值{1:.4f}".format(constr.getName(), dual, slack))
--- COPT Learning/diet problem/diet improved.py
+++ COPT Learning/diet problem/diet improved.py
@@ -1,3 +1,8 @@
+"""
+This is a COPT learning document, not for commercial use. It is mainly for recording and archiving the study and use of COPT interfaces.
+For specific examples, please refer to: https://coridm.d2d.ai/courses/32/supplement/784
+"""
+
 from coptpy import *
 
 # Create environment
@@ -28,12 +33,12 @@ allvars = model.getVars()
 print("原来的变量下界：", model.getInfo("LB", allvars))
 print("原来的变量上界：", model.getInfo("UB", allvars))
 
-#Set constriant name and Obtain original bound
+# Set constriant name and Obtain original bound
 allconstrs = model.getConstrs()
 for constr in allconstrs:
     constr_lb = model.getInfo("LB", constr)
     constr_ub = model.getInfo("UB", constr)
-    print("约束{0}的下界：{1}，上界：{2}".format(constr.getName(),constr_lb, constr_ub))
+    print("约束{0}的下界：{1}，上界：{2}".format(constr.getName(), constr_lb, constr_ub))
 
 """
 get info by vars or constrs
@@ -46,16 +51,45 @@ for constr in allconstrs:
     print("原来的约束下界：", constr.getInfo("LB"))
     print("原来的约束上界：", constr.getInfo("UB"))
 
-
 # save the basis of original variables and constraints
 varbasis = model.getVarBasis()
 conbasis = model.getConstrBasis()
 
 print("模型所有决策变量的基状态：\n")
-for var,var_ba in zip(allvars, varbasis):
+for var, var_ba in zip(allvars, varbasis):
     print("决策变量：{0}，最优值：{1:4f}，基状态为：{2}".format(var.name, var.x, var_ba))
 
 # change lower bound to 2
 model.setInfo("LB", allvars, 2)
 # change upper bound to 15
 model.setInfo("UB", allvars, 15)
+
+# Set new Bound of Vitamin_A and Vitamin_C
+# constr_A = model.getConstrByName("R(Vitamin_A)")
+# model.setInfo("LB", constr_A, 1000)
+# model.setInfo("UB", constr_A, 20000)
+# constr_C = model.getConstrByName("R(Vitamin_C)")
+# model.setInfo("LB", constr_C, 1000)
+# model.setInfo("UB", constr_C, 20000)
+
+model.setBasis(varbasis, conbasis)
+model.solve()
+
+# Analyze solution
+if model.status == COPT.OPTIMAL:
+    # Optimal objective value
+    print("最小食谱构建费用:{:.4f}".format(model.objval))
+    allvars = model.getVars()
+    #Variable value
+    print("\nValue of each variable:\n")
+    for var in allvars:
+        print("决策变量{0},最优值为 {1:.4f}，基状态为：{2}".format(var.name, var.x, var.basis))
+
+
+allconstrs = model.getConstrs()
+conbasis = model.getConstrBasis()
+print("模型所有约束条件的基状态：\n")
+for constr,constr_ba in zip(allconstrs, conbasis):
+    print("约束条件：{0}，活跃程度：{1:4f}，基状态为：{2}".format(constr.name, model.getInfo("Slack", constr), constr_ba))
+
+
