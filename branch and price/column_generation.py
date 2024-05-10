15fabc79881174ed2299d7699ce04e602823e16f
chiangwyz <1119027571@qq.com>
2024-05-10 23:25:50 +0800

    update CG file

--- branch and price/column_generation.py
+++ branch and price/column_generation.py
@@ -1,6 +1,6 @@
 import numpy as np
-import coptpy as cp
-from coptpy import COPT
+import gurobipy as grb
+from gurobipy import GRB
 from math import sqrt
 
 from color_logging import LoggerFactory
@@ -11,16 +11,23 @@ logger = LoggerFactory.get_colored_logger()
 
 # map: num --> model status
 status_map = {
-    0: "COPT.UNSTARTED",
-    1: "COPT.OPTIMAL",
-    2: "COPT.INFEASIBLE",
-    3: "COPT.UNBOUNDED",
-    4: "COPT.INF_OR_UNB",
-    5: "COPT.NUMERICAL",
-    6: "COPT.NODELIMIT",
-    7: "COPT.TIMEOUT",
-    8: "COPT.UNFINISHED",
-    9: "COPT.INTERRUPTED"
+    1: "GRB.LOADED",
+    2: "GRB.OPTIMAL",
+    3: "GRB.INFEASIBLE",
+    4: "GRB.INF_OR_UNBD",
+    5: "GRB.UNBOUNDED",
+    6: "GRB.CUTOFF",
+    7: "GRB.ITERATION_LIMIT",
+    8: "GRB.NODE_LIMIT",
+    9: "GRB.TIME_LIMIT",
+    10: "GRB.SOLUTION_LIMIT",
+    11: "GRB.INTERRUPTED",
+    12: "GRB.NUMERIC",
+    13: "GRB.SUBOPTIMAL",
+    14: "GRB.INPROGRESS",
+    15: "GRB.USER_OBJ_LIMIT",
+    16: "GRB.WORK_LIMIT",
+    17: "GRB.MEM_LIMIT"
 }
 
 
@@ -30,50 +37,43 @@ def solve_sub_problem(data, price_dual, dual_correction, branching_index: list, 
     num_types = len(price_dual)
 
     # 构造子问题
-    env = cp.Envr()
-    sub_model = env.createModel("sub problem")
+    sub_model = grb.Model("sub problem")
 
-    var_a = cp.tupledict()
+    var_a = grb.tupledict()
     for i in range(data.Customer_numbers):
-        var_a[i] = sub_model.addVar(lb=0, ub=COPT.INFINITY, vtype=COPT.INTEGER, name='a({})'.format(i))
-    # var_y = sub_model.addVars(num_types, vtype=COPT.INTEGER)
+        var_a[i] = sub_model.addVar(lb=0, ub=GRB.INFINITY, vtype=GRB.INTEGER, name='a({})'.format(i))
+    # var_y = sub_model.addVars(num_types, vtype=GRB.INTEGER)
 
     # 背包约束
     # sub_model.addConstr(var_a.prod(data.Customer_demand_sizes), COPT.LESS_EQUAL, data.Width, "width_limit")
-    sub_model.addConstr(cp.quicksum(var_a[i] * data.Customer_demand_sizes[i] for i in range(num_types)),
-                        COPT.LESS_EQUAL, data.Width, name="width constraint")
+    sub_model.addConstrs(grb.quicksum(var_a[i] * data.Customer_demand_sizes[i] for i in range(num_types)) <= data.Width, name="width constraint")
 
     # Update objective function of SUB model
-    sub_model.setObjective(1 - cp.quicksum(price_dual[i] * var_a[i] for i in range(num_types)), COPT.MINIMIZE)
+    sub_model.setObjective(1 - grb.quicksum(price_dual[i] * var_a[i] for i in range(num_types)), GRB.MINIMIZE)
 
-    sub_model.setParam(COPT.Param.Logging, 1)
-    # sub_model.setParam(COPT.Attr.PoolSols, pool_size)
-    # solution_pool = sub_model.getPoolObjVal(pool_size)
+    sub_model.setParam(GRB.Param.OutputFlag, 1)
+    sub_model.setParam(GRB.Param.PoolSearchMode, 2)
+    sub_model.setParam(GRB.Param.PoolSolutions, pool_size)
 
-    sub_model.solve()
-    sub_model.writeLp("sub_problem.lp")
+    sub_model.optimize()
+    sub_model.write("sub_problem.lp")
 
     for var in sub_model.getVars():
-        logger.info("{} = {}".format(var.name, var.x))
+        logger.info("{} = {}".format(var.VarName, var.X))
+
+    # 获取子问题求解状态
+    logger.info('sub_model.status: {}'.format(status_map[sub_model.Status]))
 
     reduced_cost = 0
-
-    logger.info('sub_model.hasmipsol: {}'.format(sub_model.hasmipsol))
-    logger.info('sub_model.haslpsol: {}'.format(sub_model.haslpsol))
-    logger.info('sub_model.status: {}'.format(sub_model.status))
-    logger.info('sub_model.poolsols: {}'.format(sub_model.getAttr("poolsols")))
-    logger.info('sub_model.PoolSols: {}'.format(sub_model.getAttr(COPT.Attr.PoolSols)))
-    print("poolsols =", sub_model.getAttr('poolsols'))
-    print("COPT.Attr.PoolSols =", sub_model.getAttr(COPT.Attr.PoolSols))
-
     pattern = []
-    for i in range(sub_model.getAttr('PoolSols')):
-        candidate_pattern = np.array(sub_model.getPoolSolution(i, var_a), dtype=np.int32)
+    for i in range(sub_model.SolCount):
+        candidate_pattern = np.array(sub_model.getAttr(GRB.Attr.Xn, sub_model.getVars()), dtype=np.int32)
+        reduced_cost = sub_model.PoolObjVal
 
-        logger.info("{}. best solution with objective value of {}".format(i+1, sub_model.getPoolObjVal(i)))
+        logger.info("{}. best solution with objective value of {}".format(i+i, sub_model.PoolObjVal))
         logger.info("candidate pattern: {}".format(candidate_pattern))
-        reduced_cost = sub_model.getPoolObjVal(i)
         logger.info("reduced_cost: {}".format(reduced_cost))
+
         if reduced_cost >= 0 or abs(reduced_cost) <= TOL:
             print("no more profitable pattern available")
             break
@@ -95,17 +95,17 @@ def solve_sub_problem(data, price_dual, dual_correction, branching_index: list, 
     return reduced_cost, new_pattern
 
 
-def solve_CSP_with_CG(data: Data, RMP_model: cp.Model, quantity_pattern, pattern, branching_index: list):
+def solve_CSP_with_CG(data: Data, RMP_model: grb.Model, quantity_pattern, pattern, branching_index: list):
     # 只做列生成并求解。
     num_types = data.Customer_numbers
     # main steps
 
-    RMP_model.solveLP()  # 求解LP
+    RMP_model.optimize()  # 求解LP
     logger.info("RMP_model status: {}".format(status_map[RMP_model.status]))
 
-    if RMP_model.status != COPT.INFEASIBLE:
+    if RMP_model.status != GRB.INFEASIBLE:
         # Get the dual values of constraints
-        dual_list = RMP_model.getInfo(COPT.Info.Dual, RMP_model.getConstrs())
+        dual_list = RMP_model.getAttr(GRB.Attr.Pi, RMP_model.getConstrs())
         price_dual = dual_list[0:num_types]  # 获取前 num_types 个约束的对偶值
         dual_correction = dual_list[num_types:len(dual_list)]  # 获取 添加的分支约束的对偶值
 
@@ -120,15 +120,17 @@ def solve_CSP_with_CG(data: Data, RMP_model: cp.Model, quantity_pattern, pattern
                 break
             for p in new_pattern:
                 # set the new pattern as a new column in the coefficient matrix
-                new_column = cp.Column(RMP_model.getConstrs()[0:num_types], p)
+                new_column = grb.Column(p, RMP_model.getConstrs()[0:num_types])
                 # add the new variable
                 quantity_pattern.append(
-                    RMP_model.addVar(obj=1.0, vtype=COPT.CONTINUOUS, column=new_column, name="add pattern" + str(p)))
+                    RMP_model.addVar(obj=1.0, vtype=GRB.CONTINUOUS, column=new_column, name="add pattern" + str(p)))
                 pattern = np.c_[pattern, p]
             # solve RMP
-            RMP_model.solveLP()
+            RMP_model.optimize()
+
+
             # get dual
-            dual_list = RMP_model.getInfo(COPT.Info.Pi, RMP_model.getConstrs())
+            dual_list = RMP_model.getAttr(GRB.Attr.Pi, RMP_model.getConstrs())
             price_dual = dual_list[0:num_types]
             dual_correction = dual_list[num_types:len(dual_list)]
 
--- branch and price/read_data.py
+++ branch and price/read_data.py
@@ -1,18 +1,22 @@
+import numpy as np
 from color_logging import LoggerFactory
 
 logger = LoggerFactory.get_colored_logger()
 
 
 class Data:
+    """
+    构造函数内初始化要声明成员变量的类型！！！
+    """
     def __init__(self):
         # 原材料钢卷长度
         self.Width = 0
 
-        # 钢卷需求的尺寸
-        self.Customer_demand_sizes = []
+        # 钢卷需求的尺寸，使用 NumPy 数组
+        self.Customer_demand_sizes = np.array([])
 
-        # 钢卷需求的数量
-        self.Customer_demands = []
+        # 钢卷需求的数量，使用 NumPy 数组
+        self.Customer_demands = np.array([])
 
         # 钢卷需求的客户数量
         self.Customer_numbers = 0
@@ -22,8 +26,8 @@ class Data:
             lines = f.readlines()
 
         self.Width = int(lines[1].strip())
-        self.Customer_demand_sizes = list(map(int, lines[3].split(', ')))
-        self.Customer_demands = list(map(int, lines[5].split(', ')))
+        self.Customer_demand_sizes = np.array(list(map(int, lines[3].split(', '))))
+        self.Customer_demands = np.array(list(map(int, lines[5].split(', '))))
 
         self.Customer_numbers = len(self.Customer_demands)
 
