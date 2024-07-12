7d6ec09b80b9b4963153ab4060703527f7eb25d3
chiangwyz <1119027571@qq.com>
2024-07-12 15:11:04 +0800

    add example

--- column generation/cutstock_CG_gurobi.py
+++ column generation/cutstock_CG_gurobi.py
@@ -44,67 +44,68 @@ def reportMIP(model):
             print(var[i].varName, " = ", var[i].x)
 
 
-try:
-    rmp = Model("rmp")
-    sub = Model("sub")
+if __name__ == "__main__":
+    try:
+        rmp = Model("rmp")
+        sub = Model("sub")
 
-    rmp.setParam("OutputFlag", 0)
-    sub.setParam("OutputFlag", 0)
+        rmp.setParam("OutputFlag", 0)
+        sub.setParam("OutputFlag", 0)
 
-    # construct RMP
-    rmp_var = []
-    for i in range(nwidth):
-        rmp_var.append(rmp.addVar(0.0, GRB.INFINITY, 1.0, GRB.CONTINUOUS, "rmp_" + str(i)))
+        # construct RMP
+        rmp_var = []
+        for i in range(nwidth):
+            rmp_var.append(rmp.addVar(0.0, GRB.INFINITY, 1.0, GRB.CONTINUOUS, "rmp_" + str(i)))
 
-    rmp_con = []
-    row_coeff = [0.0] * nwidth
-    for i in range(nwidth):
-        row_coeff[i] = int(rollwidth / size[i])
-        rmp_con.append(rmp.addConstr(quicksum(rmp_var[j] * row_coeff[j] for j in range(nwidth)) >= amount[i], "rmpcon_" + str(i)))
-        row_coeff[i] = 0.0
+        rmp_con = []
+        row_coeff = [0.0] * nwidth
+        for i in range(nwidth):
+            row_coeff[i] = int(rollwidth / size[i])
+            rmp_con.append(rmp.addConstr(quicksum(rmp_var[j] * row_coeff[j] for j in range(nwidth)) >= amount[i], "rmpcon_" + str(i)))
+            row_coeff[i] = 0.0
 
-    rmp.setAttr("ModelSense", GRB.MINIMIZE)
-    # end RMP
+        rmp.setAttr("ModelSense", GRB.MINIMIZE)
+        # end RMP
 
-    # construct SUB
-    sub_var = []
-    for i in range(nwidth):
-        sub_var.append(sub.addVar(0.0, GRB.INFINITY, 0.0, GRB.INTEGER, "sub_" + str(i)))
+        # construct SUB
+        sub_var = []
+        for i in range(nwidth):
+            sub_var.append(sub.addVar(0.0, GRB.INFINITY, 0.0, GRB.INTEGER, "sub_" + str(i)))
 
-    sub.addConstr(quicksum(sub_var[i] * size[i] for i in range(nwidth)) <= rollwidth, "subcon")
-    # end SUB
+        sub.addConstr(quicksum(sub_var[i] * size[i] for i in range(nwidth)) <= rollwidth, "subcon")
+        # end SUB
 
-    print("               *** Column Generation Loop ***               \n")
-    for i in range(MAX_CGTIME):
-        print("Iteration: ", i, "\n")
+        print("               *** Column Generation Loop ***               \n")
+        for i in range(MAX_CGTIME):
+            print("Iteration: ", i, "\n")
+
+            rmp.optimize()
+            reportRMP(rmp)
+
+            rmp_pi = rmp.getAttr("Pi", rmp.getConstrs())
+
+            sub.setObjective(1 - quicksum(sub_var[i] * rmp_pi[i] for i in range(nwidth)), GRB.MINIMIZE)
+            sub.optimize()
+            reportSUB(sub)
+
+            if sub.objVal > -1e-6:
+                break
+
+            rmp_coeff = sub.getAttr("X", sub.getVars())
+            rmp_col = Column(rmp_coeff, rmp_con)
+            rmp.addVar(0.0, GRB.INFINITY, 1.0, GRB.CONTINUOUS, "cg_" + str(i), rmp_col)
+
+        print("               *** End Loop ***               \n")
+
+        mip_var = rmp.getVars()
+        for i in range(rmp.numVars):
+            mip_var[i].setAttr("VType", GRB.INTEGER)
 
         rmp.optimize()
-        reportRMP(rmp)
+        reportMIP(rmp)
 
-        rmp_pi = rmp.getAttr("Pi", rmp.getConstrs())
+    except GurobiError as e:
+        print('Error code ' + str(e.errno) + ": " + str(e))
 
-        sub.setObjective(1 - quicksum(sub_var[i] * rmp_pi[i] for i in range(nwidth)), GRB.MINIMIZE)
-        sub.optimize()
-        reportSUB(sub)
-
-        if sub.objVal > -1e-6:
-            break
-
-        rmp_coeff = sub.getAttr("X", sub.getVars())
-        rmp_col = Column(rmp_coeff, rmp_con)
-        rmp.addVar(0.0, GRB.INFINITY, 1.0, GRB.CONTINUOUS, "cg_" + str(i), rmp_col)
-
-    print("               *** End Loop ***               \n")
-
-    mip_var = rmp.getVars()
-    for i in range(rmp.numVars):
-        mip_var[i].setAttr("VType", GRB.INTEGER)
-
-    rmp.optimize()
-    reportMIP(rmp)
-
-except GurobiError as e:
-    print('Error code ' + str(e.errno) + ": " + str(e))
-
-except AttributeError:
-    print('Encountered an attribute error')
+    except AttributeError:
+        print('Encountered an attribute error')
